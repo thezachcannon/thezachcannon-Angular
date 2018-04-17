ed2c4be7892599448a4b506c13d24b957f141b2f
thezachcannon <zachary.cannon@hotmail.com>
2018-04-17 15:25:11 -0500

    More updates to UI/ Resume Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+//Vendor
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { AboutComponent } from './components/about/about.component';
+import { CodeComponent } from './components/code/code.component';
 
+
+
+
+const appRoutes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'resume',
+    component: ResumeComponent
+  },
+  {
+    path: 'code',
+    component: CodeComponent
+  },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ResumeComponent,
+    AboutComponent,
+    CodeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PdfViewerModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
